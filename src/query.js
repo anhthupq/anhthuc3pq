e8b9bf58f7c77ab38e09b7679be056c484645e89
kietluong <tuankietpq@gmail.com>
2021-12-03 20:09:39 +0700

    fix

--- src/query.js
+++ src/query.js
@@ -54,7 +54,7 @@ router.get('/search', async function (req, res) {
 
       }
   }
-    body = { query }
+  }
   try {
     const { body: { hits } } = await client.search({
       from: req.page || 0,
